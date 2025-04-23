4627738a17f3e622d1b6b23a2a561602f02641ac
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-23 10:35:19 +0400

    Addition The  Function Types Of The  User Menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MiniBankSystemProjectOverview
+﻿using System;
+
+namespace MiniBankSystemProjectOverview
 {
     internal class Program
 
@@ -14,7 +16,7 @@
         static List<int> accountNumbers = new List<int>();
         static List<string> accountNames = new List<string>();
         static List<double> balances = new List<double>();
-        static List<string> Passward = new List<string>();
+        
 
 
         // _____Queues and Stacks______
@@ -42,6 +44,7 @@
                     Console.WriteLine("\n====== Welcome To Bank System ======");
                     Console.WriteLine("1. User Menu");
                     Console.WriteLine("2. Admin Menu");
+                    Console.WriteLine("3. Employee Menu");
                     Console.WriteLine("0. Exit the system");
                     Console.Write(" Please Select Your Option: ");
                     string mainChoice = Console.ReadLine();
@@ -50,22 +53,56 @@
                     {
                         case "1": UserMenu(); break;
                         case "2": AdminMenu(); break;
-                        case "0":
+                        case "3": EmployeeMenu(); break;
+                        case "0": ExitTheSystem(); break;
+
                             SaveAccountsInformationToFile();
                             SaveReviews();
-                            running = false;
+                            running = false; //____Keep running  false to repeat the loop____
                             break;
                         default: Console.WriteLine("Invalid choice."); break;
                     }
                 }
 
-                Console.WriteLine("Do you want another operation ? y / n");
+                Console.WriteLine("Do You Want Another Option  ? y / n");
                 choice = Console.ReadKey().KeyChar;
 
             } while (choice == 'y' || choice == 'Y');
             Console.WriteLine("Thank you for using Bank System!");
         }
 
+        // ==========  Addition The  Function Types Of The  User Menu==========
+        static void UserMenu()
+        {
+            bool inUserMenu = true;
+
+            while (inUserMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("\n------ User Menu ------");
+                Console.WriteLine("1. Request Account Creation");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. View Balance");
+                Console.WriteLine("5. Submit Review/Complaint");
+                Console.WriteLine("6. Manage personal details");
+                Console.WriteLine("0. Return to Main Menu");
+                Console.Write("Select option: ");
+                string userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1": RequestAccountCreation(); break;
+                    case "2": Deposit(); break;
+                    case "3": Withdraw(); break;
+                    case "4": ViewBalance(); break;
+                    case "5": SubmitReview(); break;
+                    case "6": Managepersonaldetails(); break;
+                    case "0": inUserMenu = false; break;
+                    default: Console.WriteLine("Invalid choice."); break;
+                }
+            }
+        }
 
 
 
