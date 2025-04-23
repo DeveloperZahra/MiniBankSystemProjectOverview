779612e421b9c00147a839c71ddc2666e95dae86
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-23 09:55:23 +0400

    add  the main menu for system bank

--- Program.cs
+++ Program.cs
@@ -26,6 +26,48 @@
         // _____Account number generator_____
         static int lastAccountNumber;
 
+        static void Main(string[] args)
+        {
+            LoadAccountsInformationFromFile();
+            LoadReviews();
+
+            // _______main menu for system bank to store user choice in avriable _______
+            bool running = true;
+            char choice;
+            do
+            {
+                while (running)
+                {
+                    Console.Clear();//____just to clear the screen____
+                    Console.WriteLine("\n====== Welcome To Bank System ======");
+                    Console.WriteLine("1. User Menu");
+                    Console.WriteLine("2. Admin Menu");
+                    Console.WriteLine("0. Exit the system");
+                    Console.Write(" Please Select Your Option: ");
+                    string mainChoice = Console.ReadLine();
+
+                    switch (mainChoice)
+                    {
+                        case "1": UserMenu(); break;
+                        case "2": AdminMenu(); break;
+                        case "0":
+                            SaveAccountsInformationToFile();
+                            SaveReviews();
+                            running = false;
+                            break;
+                        default: Console.WriteLine("Invalid choice."); break;
+                    }
+                }
+
+                Console.WriteLine("Do you want another operation ? y / n");
+                choice = Console.ReadKey().KeyChar;
+
+            } while (choice == 'y' || choice == 'Y');
+            Console.WriteLine("Thank you for using Bank System!");
+        }
+
+
+
 
 
 
