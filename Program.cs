d9be63f673e419cf949e76705121327af288b7e5
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-04-23 08:44:36 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniBankSystemProjectOverview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
